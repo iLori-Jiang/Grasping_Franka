9887e799dfed1b499f9852996a96ce5b91fb1e8e
iLori <50691494+iLori-Jiang@users.noreply.github.com>
2021-07-05 14:49:51 +0800

    Add files via upload

--- Utils/get_obj_by_color.py
+++ Utils/get_obj_by_color.py
@@ -17,8 +17,6 @@ def get_obj_bbox(img, y1, y2, x1, x2):
     gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     blur = cv2.blur(gray, (4, 4))
     _, binary = cv2.threshold(blur, 130, 256, cv2.THRESH_BINARY)
-    # cntrs, _ = cv2.findContours(binary, 1, 2)
-    # cnt = cntrs[0]
 
     x, y, w, h = cv2.boundingRect(binary)
 
@@ -40,16 +38,12 @@ def get_obj_bbox(img, y1, y2, x1, x2):
 
 
 def check_gripper_bbox(img, y1, y2, x1, x2):
-    # crop_offset_col = 300
-    # crop_offset_row = 100
 
     crop = img[y1:y2, x1:x2]
 
     gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     blur = cv2.blur(gray, (4, 4))
     _, binary = cv2.threshold(blur, 130, 256, cv2.THRESH_BINARY)
-    # cntrs, _ = cv2.findContours(binary, 1, 2)
-    # cnt = cntrs[0]
 
     x, y, w, h = cv2.boundingRect(binary)
 
@@ -75,25 +69,20 @@ if __name__ == '__main__':
     """ Initialization """
     # camera and robot driver
     print('work_dir: ', _root_path)
-    cam = Realsense(frame_width=1280, frame_height=720, fps=30)
+    cam = Realsense(frame_width=1280, frame_height=720, fps=30, exposure=800, white_balance=3500, contrast=80, sharpness=80)
 
     _, img = cam.get_frame_cv()
 
-    # crop_offset_col = 300
-    # crop_offset_row = 100
-
-    y1 = 147
-    y2 = 516
-    x1 = 424
-    x2 = 857
+    y1 = 269
+    y2 = 542
+    x1 = 475
+    x2 = 799
 
     crop = img[y1:y2, x1:x2]
 
     gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     blur = cv2.blur(gray, (4, 4))
     _, binary = cv2.threshold(blur, 130, 256, cv2.THRESH_BINARY)
-    # cntrs, _ = cv2.findContours(binary, 1, 2)
-    # cnt = cntrs[0]
 
     x, y, w, h = cv2.boundingRect(binary)
 
